7af5fc118e05aadb8749cb65257f5dc38ffd5996
Matt Crook <matt.crook@getbuilt.com>
2021-05-14 18:32:19 -0500

    wip: edit to outputs.

--- aws/webserver_cluster_with_modules/live/global/s3/outputs.tf
+++ aws/webserver_cluster_with_modules/live/global/s3/outputs.tf
@@ -17,7 +17,7 @@ output "brave_arn" {
      # value       = aws_iam_user.example_iam[*].arn
 
      # When resouce is a map (using for_each)
-     value       = aws_iam_user.example_iam
+     # value       = aws_iam_user.example_iam
 
      # Output each arn within the map.
      value       = values(aws_iam_user.example_iam[*].arn)
