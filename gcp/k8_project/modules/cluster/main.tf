af8048caf694ec10afb2c81701504b4a6f38cf86
Matt Crook <matt.crook@getbuilt.com>
2021-05-28 09:24:23 -0500

    wip: kubernetes cluster set up.

--- gcp/k8_project/modules/cluster/main.tf
+++ gcp/k8_project/modules/cluster/main.tf
@@ -35,3 +35,50 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     ]
   }
 }
+
+-------
+
+resource "google_container_cluster" "default" {
+  name        = var.cluster_name
+  project     = var.project_id
+  description = var,description
+  location    = var.region
+
+# By default, creating a cluster creates a default node pool as it requires one to be created, we are removing this node as we are ceating our own resource below.
+  remove_default_node_pool = true
+  initial_node_count       = var.initial_node_count
+
+  master_auth {
+    username = var.username
+    password = var.password
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "default" {
+  name       = "${var.name}-node-pool"
+  project    = var.project_id
+  location   = var.region
+  cluster    = google_container_cluster.default.name
+  node_count = var.node_count
+
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.gke-sa.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
--- gcp/k8_project/modules/cluster/vars.tf
+++ gcp/k8_project/modules/cluster/vars.tf
@@ -21,3 +21,19 @@ variable "node_pool_region" {
   type        = string
   default     = "us-central1"
 }
+
+variable "name" {
+  description = "The namw of the node pool"
+  type        = string
+  default     = "k8-cluster-project"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the node pool"
+  type        = number
+  default     = 3
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
