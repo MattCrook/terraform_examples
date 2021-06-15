a04d0b32e57507cd07e26166b90d798c6243f3b3
Matt Crook <matt.crook@getbuilt.com>
2021-06-15 10:22:08 -0500

    wip: added to examples directory provisioners and external data sources.

---
+++ extra/more_than_tf/remote_exec_provisioner.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+# Generate a private key in Terraform.
+# In real-world usage, you should manage SSH keys outside of Terraform.
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Pirvate key stored in TF state, not for production, but fine as an exercise. 
+resource "aws_key_pair" "generated_key" {
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+# New attribute is key_name - used to instruct AWS to associate your public key with this EC2 instance.
+resource "aws_instance" "example" {
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = aws_key_pair.generated_key.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo \"Hello, World from $(uname -smp)\""]
+  }
+  # Need to tell Terraform to use SSH to connect to this EC2 instance when running the remote-exec provisioner.
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = tls_private_key.example.private_key_pem
+  }
+
+}
+
+resource "aws_security_group" "instance" {
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    # To make this example easy to try out, we allow all SSH connections.
+    # In real world usage, you should lock this down to solely trusted IPs.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
